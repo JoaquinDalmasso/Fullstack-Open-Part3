55f743357fe7f156346c1b455e427211f961a004
JoaquinDalmasso <joacodalmasso@gmail.com>
2024-01-23 21:31:26 -0300

    Prueba de contraseña

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose')
 
 const password = process.argv[2]
 const url = 
-    `mongodb+srv://fullstack:${password}@fullstackopen.j10dt.mongodb.net/persons-app?retryWrites=true&w=majority`
+    `mongodb+srv://fullstack:123@fullstackopen.j10dt.mongodb.net/persons-app?retryWrites=true&w=majority`
 
 mongoose.connect(url)
 
